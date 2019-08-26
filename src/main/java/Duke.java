7d1116224fc3a66828f30569e26bf958ce9f0f72
joanneong <joanneongcf@u.nus.edu>
2019-08-26 13:45:46 +0800

    Implement Level-3 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,14 +12,17 @@ public class Duke {
     private static String GREETING = "    Hello! I'm Duke\n"
             +  "    What can I do for you?\n";
 
+    private static String DONE = "done";
     private static String LIST = "list";
     private static String BYE = "bye";
 
+    private static String DONE_MESSAGE = "Nice! I've marked this task as done:";
+    private static String LIST_MESSAGE = "Here are the tasks in your list:";
     private static String BYE_MESSAGE = "Bye. Hope to see you again soon!";
 
     private static String INDENTATION = "    ";
 
-    private static String[] commands = new String[100];
+    private static Task[] commands = new Task[100];
     private static int no_of_commands = 0;
 
     public static void greet_user() {
@@ -28,9 +31,38 @@ public class Duke {
         System.out.println(GREETING);
     }
 
+    public static void check_task(String command) {
+        String[] splitCommand = command.split(" ");
+        if (splitCommand.length == 2) {
+            int idx = Integer.parseInt(splitCommand[1]) - 1;
+
+            if (idx >= 0 && idx < no_of_commands) {
+                commands[idx].markDone();
+
+                System.out.println(INDENTATION + DONE_MESSAGE);
+                System.out.println(INDENTATION + "[" + commands[idx].getStatusIcon() + "] " + commands[idx].getDescription());
+            }
+        }
+    }
+
+    public static void print_list() {
+        System.out.println(LIST_MESSAGE);
+        for (int i = 0; i < no_of_commands; i++) {
+            Task task = commands[i];
+            System.out.println(i + 1 + ". [" + task.getStatusIcon() + "] " + task.getDescription());
+        }
+    }
+
     public static void print_goodbye() {
         System.out.println(INDENTATION + BYE_MESSAGE);
         System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void add_command(String command) {
+        Task newTask = new Task(command);
+        commands[no_of_commands] = newTask;
+        no_of_commands++;
+        System.out.println(INDENTATION + "added: " + command);
     }
 
     public static void process_commands() {
@@ -41,17 +73,15 @@ public class Duke {
             String command = sc.nextLine();
             System.out.println(HORIZONTAL_LINE);
 
-            if (command.equals(LIST)) {
-                for (int i = 0; i < no_of_commands; i++) {
-                    System.out.println(i + 1 + ". " + commands[i]);
-                }
+            if (command.contains(DONE)) {
+                check_task(command);
+            } else if (command.equals(LIST)) {
+                print_list();
             } else if (command.equals(BYE)) {
                 print_goodbye();
                 break;
             } else {
-                commands[no_of_commands] = command;
-                no_of_commands++;
-                System.out.println(INDENTATION + "added: " + command);
+                add_command(command);
             }
         }
     }
